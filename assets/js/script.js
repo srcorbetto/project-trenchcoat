00d31f508a3c132435a2e1dccd617fc5fc2328fb
Mike Corbett <srcorbetto@gmail.com>
2018-01-15 22:32:15 -0600

    file upload working. Just need to clean up the process

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,61 @@ $(document).ready(function(){
     authDomain: "project-trenchcoat.firebaseapp.com",
     databaseURL: "https://project-trenchcoat.firebaseio.com",
     projectId: "project-trenchcoat",
-    storageBucket: "",
+    storageBucket: "project-trenchcoat.appspot.com",
     messagingSenderId: "404151361992"
   };
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
-  $("#form-submit").click(function(e){
+  // Selector for the file input
+  var imageInput = document.getElementById("input-image");
 
-    e.preventDefault();
+  // Process of uploading the image
+  imageInput.addEventListener("change", function(e){
 
-    var fileUpload = $("#input-image").val();
+    // Get file
+    var file = e.target.files[0];
 
-    console.log(fileUpload);
+    // Create a storage reference
+    var storageRef = firebase.storage().ref("user_images/" + file.name);
 
-    database.ref().push(fileUpload);
+    // Upload File
+    var task = storageRef.put(file);
+
+
+    // Update progress
+    task.on("state_changed",
+
+      function progress(snapshot) {
+
+        console.log(snapshot);
+
+      },
+
+      function error(err) {
+
+        console.log(err);
+
+      },
+
+      function complete() {
+
+        console.log("complete");
+
+        //Once complete - get the stored image URL
+        storageRef.getDownloadURL().then(function(url){
+
+          console.log(url);
+
+          $(".card-img-top").attr("src", url);
+
+        });
+
+      }
+
+      );
 
   });
 
-});
+  });
