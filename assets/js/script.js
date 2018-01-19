12bf1daa110683ccf44ad8f9852ec8a5ac0469cf
Mike Corbett <srcorbetto@gmail.com>
2018-01-18 21:22:53 -0600

    Impression meter

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,72 @@ $(document).ready(function(){
   // Selector for the file input
   var imageInput = document.getElementById("input-image");
 
+  //Face Impression value
+  var impressionScore;
+
+  // Face Click
+  $("#face1").click(function() {
+
+    $(this).toggleClass("face1-active");
+    $("#face2").removeClass('face2-active');
+    $("#face3").removeClass('face3-active');
+    $("#face4").removeClass('face4-active');
+    $("#face5").removeClass('face5-active');
+
+    impresssionScore = 33;
+    console.log(impresssionScore);
+
+  });
+  $("#face2").click(function() {
+
+    $(this).toggleClass("face2-active");
+    $("#face1").removeClass('face1-active');
+    $("#face3").removeClass('face3-active');
+    $("#face4").removeClass('face4-active');
+    $("#face5").removeClass('face5-active');
+
+    impresssionScore = 24.75;
+    console.log(impresssionScore);
+
+  });
+  $("#face3").click(function() {
+
+    $(this).toggleClass("face3-active");
+    $("#face2").removeClass('face2-active');
+    $("#face1").removeClass('face1-active');
+    $("#face4").removeClass('face4-active');
+    $("#face5").removeClass('face5-active');
+
+    impresssionScore = 16.5;
+    console.log(impresssionScore);
+
+  });
+  $("#face4").click(function() {
+
+    $(this).toggleClass("face4-active");
+    $("#face2").removeClass('face2-active');
+    $("#face3").removeClass('face3-active');
+    $("#face1").removeClass('face1-active');
+    $("#face5").removeClass('face5-active');
+
+    impresssionScore = 8.25;
+    console.log(impresssionScore);
+
+  });
+  $("#face5").click(function() {
+
+    $(this).toggleClass("face5-active");
+    $("#face2").removeClass('face2-active');
+    $("#face3").removeClass('face3-active');
+    $("#face4").removeClass('face4-active');
+    $("#face1").removeClass('face1-active');
+
+    impresssionScore = 0;
+    console.log(impresssionScore);
+
+  });
+
+
   // Process of uploading the image
   $("#form-submit").on("click", function(e){
     e.preventDefault();
