2ca2162a55c6c76126baf95e8fe378b3fa3ac580
Your Name <nbos0531@gmail.com>
2018-01-20 11:24:38 -0600

    various changes to fix things

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,182 +97,214 @@ $(document).ready(function(){
   // Process of uploading the image
   $("#form-submit").on("click", function(e){
     e.preventDefault();
+    $('#modal').css('display', 'block');
+    $('body').css('background-color', 'grey');
+
+    $('#goBack').on('click', function(event){
+      event.preventDefault(); 
+      $('#modal').css('display', 'none');
+      $('body').css('background-color', 'white');
+
+    });
+    $('#Proceed').on('click', function(event) {
+
+      event.preventDefault(); 
+      $('#modal').css('display', 'none');
+      $('body').css('background-color', 'white');
 
 
 
-    var str1 = $("#input-text").val().trim();
-    console.log(str1);
-    var longestWord = "";
-    var totalLetters = 0;
-    var wordLengthAverage = 0;
-    str1 = str1.replace(/[\r\n]+/g," ");
-    var textArray = str1.split(" ");
+      var str1 = $("#input-text").val().trim();
+      console.log(str1);
+      var longestWord = "";
+      var totalLetters = 0;
+      var wordLengthAverage = 0;
+      str1 = str1.replace(/[\r\n]+/g," ");
+      var textArray = str1.split(" ");
 
-    for (var i=0; i<textArray.length;i++){
-        textArray[i] = textArray[i].replace(/[^a-zA-Z]+/g,""); 
-        // Average word length calculation
-        totalLetters = totalLetters + textArray[i].length;
-        wordLengthAverage = totalLetters/(i+1);
-        // Find the longest word and save it and display it
-        if (textArray[i].length > longestWord.length){
-            longestWord = textArray[i];
-            console.log("Longest word: " + longestWord);
-        };
-    };
+      for (var i=0; i<textArray.length;i++){
+          textArray[i] = textArray[i].replace(/[^a-zA-Z]+/g,""); 
+          // Average word length calculation
+          totalLetters = totalLetters + textArray[i].length;
+          wordLengthAverage = totalLetters/(i+1);
+          // Find the longest word and save it and display it
+          if (textArray[i].length > longestWord.length){
+              longestWord = textArray[i];
+              console.log("Longest word: " + longestWord);
+          };
+      };
 
-    $("#textInfo").append("<br>" + "Average Word Length: " + wordLengthAverage.toFixed(3)); 
-    $("#textInfo").append("<br>" + "Longest Word: " + longestWord + ", " + longestWord.length + " letters");
-
-
+      $("#textInfo").append("<br>" + "Average Word Length: " + wordLengthAverage.toFixed(3)); 
+      $("#textInfo").append("<br>" + "Longest Word: " + longestWord + ", " + longestWord.length + " letters");
 
 
 
-    // Get file
-    var file = imageInput.files[0];
-
-    var imageURL = $("#input-url").val().trim();
-    console.log(imageURL);
 
 
-    // if (imageURL === "") {
+      // Get file
+      var file = imageInput.files[0];
 
-    //   console.log('this is working');
-
-    //       // Get file
-    //   var file = imageInput.files[0];
-    //   console.log(file);
-
-    //   // Create a storage reference
-    //   var storageRef = firebase.storage().ref("user_images/" + file.name);
-
-    //   // Upload File
-    //   var task = storageRef.put(file);
-
-    //   var newURL = '';
-
-    //   // Update progress
-    //   task.on("state_changed",
-
-    //     function progress(snapshot) {
-
-    //       console.log(snapshot);
-
-    //     },
-
-    //     function error(err) {
-
-    //       console.log(err);
-
-    //     },
-
-    //     function complete() {
-
-    //       console.log("complete");
-
-    //       //Once complete - get the stored image URL
-    //       storageRef.getDownloadURL().then(function(url){
-
-    //       newURL = url; 
-
-    //       console.log(url);
-    //       console.log(newURL);
-
-    //       $(".profile").attr("src", url);
-    //       var cors_api_host = 'cors-anywhere.herokuapp.com';
+      var imageURL = $("#input-url").val().trim();
+      console.log(imageURL);
 
 
-    //       // Gather link to image
+      if (imageURL === "") {
 
-    //       var source = newURL;
-    //       console.log(source);
+        console.log('this is working');
+
+            // Get file
+        var file = imageInput.files[0];
+        console.log(file);
+
+        // Create a storage reference
+        var storageRef = firebase.storage().ref("user_images/" + file.name);
+
+        // Upload File
+        var task = storageRef.put(file);
+
+        var newURL = '';
+
+        // Update progress
+        task.on("state_changed",
+
+          function progress(snapshot) {
+
+            console.log(snapshot);
+
+          },
+
+          function error(err) {
+
+            console.log(err);
+
+          },
+
+          function complete() {
+
+            console.log("complete");
+
+            //Once complete - get the stored image URL
+            storageRef.getDownloadURL().then(function(url){
+
+            newURL = url; 
+
+            console.log(url);
+            console.log(newURL);
+
+            $(".profile").attr("src", url);
+            var cors_api_host = 'cors-anywhere.herokuapp.com';
 
 
-    //       //header keys
-    //       var headers = {
-    //         "app_id"          : "59fe3f8b",
-    //         "app_key"         : "472b1119c610482500bfb00b6e897a1c"
-    //       };
-          
-    //       //face recognition api call
-    //       var urlFace = "http://" + cors_api_host +  "/http://api.kairos.com/detect";
-    //       var payload  = { "image" : source };
-    //       console.log(payload);
-    //       console.log(urlFace);
-    //       $.ajax(urlFace, {
-    //         headers  : headers,
-    //           type: "POST",
-    //           data: JSON.stringify(payload),
-    //           dataType: "JSON"
-    //       }).done(function(response){
+            // Gather link to image
+
+            var source = newURL;
+            console.log(source);
+
+
+            //header keys
+            var headers = {
+              "app_id"          : "59fe3f8b",
+              "app_key"         : "472b1119c610482500bfb00b6e897a1c"
+            };
             
-    //         console.log(response);
-    //         //console.log(response.images[0].faces[0].height);
-    //       });
+            //face recognition api call
+            var request = new XMLHttpRequest();
 
-    //       //emotion api call
-    //       var url = "http://" + cors_api_host + '/https://api.kairos.com/v2/media?source=' + source ;
-    //       $.ajax(url, {
-    //         headers  : headers,
-    //           type: "POST",
-    //           dataType: "JSON"
-    //       }).done(function(response){
+          request.open('POST', 'http://cors-anywhere.herokuapp.com/https://api.kairos.com/detect');
+
+          request.setRequestHeader('app_id', '59fe3f8b');
+          request.setRequestHeader('app_key', '472b1119c610482500bfb00b6e897a1c');
+          request.setRequestHeader('Content-Type', 'application/JSON');
+
+          request.onreadystatechange = function () {
+            if (this.readyState === 4) {
+              console.log('Status:', this.status);
+              console.log('Headers:', this.getAllResponseHeaders());
+              console.log(JSON.parse(this.responseText));
+            }
+          };
+
+            var body = {
+              'image' : source
+            }
+            console.log('this is correct');
+            request.send(JSON.stringify(body));
             
-    //         console.log(response);
+
+            //emotion api call
+            var url = "http://" + cors_api_host + '/https://api.kairos.com/v2/media?source=' + source ;
+            $.ajax(url, {
+              headers  : headers,
+                type: "POST",
+                dataType: "JSON"
+            }).done(function(response){
+              
+              console.log(response);
+              
+            });
+
+            });
+
+          //end complete function  
+          });
+      }
+      else {
+
+
+        var cors_api_host = 'cors-anywhere.herokuapp.com';
+        var source = $("#input-url").val().trim();
+        $('.profile').attr('src', source);
+
+
+            //header keys
+            var headers = {
+              "app_id"          : "59fe3f8b",
+              "app_key"         : "472b1119c610482500bfb00b6e897a1c"
+            };
             
-    //       });
+             //face recognition api call
+            var request = new XMLHttpRequest();
 
-    //       });
+            request.open('POST', 'http://cors-anywhere.herokuapp.com/https://api.kairos.com/detect');
 
-    //     //end complete function  
-    //     });
-    // }
-    // else {
+            request.setRequestHeader('app_id', '59fe3f8b');
+            request.setRequestHeader('app_key', '472b1119c610482500bfb00b6e897a1c');
+            request.setRequestHeader('Content-Type', 'application/JSON');
 
-    //   var cors_api_host = 'cors-anywhere.herokuapp.com';
-    //   var source = $("#input-url").val().trim();
+            request.onreadystatechange = function () {
+              if (this.readyState === 4) {
+                console.log('Status:', this.status);
+                console.log('Headers:', this.getAllResponseHeaders());
+                console.log(JSON.parse(this.responseText));
+              }
+            };
 
+            var body = {
+              'image' : source
+            }
+            console.log('this is correct');
+            request.send(JSON.stringify(body));
 
-    //       //header keys
-    //       var headers = {
-    //         "app_id"          : "59fe3f8b",
-    //         "app_key"         : "472b1119c610482500bfb00b6e897a1c"
-    //       };
-          
-    //       //face recognition api call
-    //       var urlFace = "http://" + cors_api_host +  "/http://api.kairos.com/detect";
-    //       var payload  = { "image" : source };
-    //       console.log(payload);
-    //       console.log(urlFace);
-    //       $.ajax(urlFace, {
-    //         headers  : headers,
-    //           type: "POST",
-    //           data: JSON.stringify(payload),
-    //           dataType: "JSON"
-    //       }).done(function(response){
-            
-    //         console.log(response);
-    //         //console.log(response.images[0].faces[0].height);
-    //       });
+            //emotion api call
+            var url = "http://" + cors_api_host + '/https://api.kairos.com/v2/media?source=' + source ;
+            $.ajax(url, {
+              headers  : headers,
+                type: "POST",
+                dataType: "JSON"
+            }).done(function(response){
+              
+              console.log(response);
+              
+            });
 
-    //       //emotion api call
-    //       var url = "http://" + cors_api_host + '/https://api.kairos.com/v2/media?source=' + source ;
-    //       $.ajax(url, {
-    //         headers  : headers,
-    //           type: "POST",
-    //           dataType: "JSON"
-    //       }).done(function(response){
-            
-    //         console.log(response);
-            
-    //       });
-
-    // }
+      }
 
 
 
     indexChart();
 
       // end on-click
+      });
       });
 
   // });  
